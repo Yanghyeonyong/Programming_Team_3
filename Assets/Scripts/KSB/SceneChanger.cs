2c43aabe63859016d4a4572195fb86fffefd93a6
BADABAMDOOG <sbya1228@gmail.com>
2025-10-31 06:02:31 +0900

    Add SceneChanger

--- Assets/Scripts/KSB/SceneChanger.cs
+++ Assets/Scripts/KSB/SceneChanger.cs
@@ -1,19 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class SceneChanger : MonoBehaviour
 {
-    public void Load(int sceneNumber)
+
+    public static SceneChanger Instance { get; private set; }
+
+    void Awake()
     {
-        SceneManager.LoadScene(sceneNumber);
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
 
-    public void Load(string sceneName)
+    public void LoadByName(string sceneName)
     {
+        if (string.IsNullOrEmpty(sceneName)) return;
         SceneManager.LoadScene(sceneName);
+
     }
-   
+
+
+    public void LoadByIndex(int sceneIndex)
+    {
+        int count = SceneManager.sceneCountInBuildSettings;
+        if (sceneIndex < 0 || sceneIndex >= count) return;
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+
+    public void LoadNext()
+    {
+        int count = SceneManager.sceneCountInBuildSettings;
+        if (count == 0) return;
+
+        int cur = SceneManager.GetActiveScene().buildIndex;
+        int next = (cur < count - 1) ? cur + 1 : 0;
+        SceneManager.LoadScene(next);
+    }
+
+
+    public void LoadPrev()
+    {
+        int count = SceneManager.sceneCountInBuildSettings;
+        if (count == 0) return;
+
+        int cur = SceneManager.GetActiveScene().buildIndex;
+        int prev = (cur > 0) ? cur - 1 : count - 1;
+        SceneManager.LoadScene(prev);
+    }
+
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
